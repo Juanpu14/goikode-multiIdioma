7f11228113c18acdc6949e0ff5fd566fb998ef3b
Juanpu14 <juanpu2004@gmail.com>
2025-05-13 14:10:06 +0200

    prueba: funcione pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/goikode-multiIdioma/', // 👈 añade esto
+  base: '/goikode-multiIdioma/', 
   plugins: [vue()],
 })
